41a17872bba52d65eaf5cb6519e9414f9d58d6df
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2018-12-26 23:37:24 -0700

    Adding another TestCase.

--- setest_firstdeclension.py
+++ setest_firstdeclension.py
@@ -9,9 +9,22 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class ShortestPathToWinTestCase(unittest.TestCase):
+class LudiTestCase(unittest.TestCase):
+
+    def _perform_get(self):
+        self.driver.get('file://%s/first-declension.html' % os.getcwd())
+
+    def _assert_id_is_not_displayed(self, element_id):
+        self.assertFalse(
+            self.driver.find_element_by_id(element_id).is_displayed())
+
+    def _assert_id_is_displayed(self, element_id):
+        self.assertTrue(
+            self.driver.find_element_by_id(element_id).is_displayed())
+
+class DirectWinTestCase(LudiTestCase):
     '''
-    Exercise shortest path to winning the game.
+    Exercise the shortest path to winning the game.
     '''
 
     def test_chrome(self):
@@ -31,17 +44,6 @@ class ShortestPathToWinTestCase(unittest.TestCase):
         finally:
             self.driver.quit()
 
-    def _assert_id_is_not_displayed(self, element_id):
-        self.assertFalse(
-            self.driver.find_element_by_id(element_id).is_displayed())
-
-    def _assert_id_is_displayed(self, element_id):
-        self.assertTrue(
-            self.driver.find_element_by_id(element_id).is_displayed())
-
-    def _perform_get(self):
-        self.driver.get('file://%s/first-declension.html' % os.getcwd())
-
     def _perform_clicks(self):
         element_ids = (
             'start-button',
@@ -53,6 +55,57 @@ class ShortestPathToWinTestCase(unittest.TestCase):
             'tile-ae.2',
             'tile-arum',
             'tile-is.0',
+            'tile-as',
+            'tile-is.1',
+        )
+        for element_id in element_ids:
+            self.driver.find_element_by_id(element_id).click()
+
+class MistakeAtEndTestCase(LudiTestCase):
+    '''
+    Make a mistake at the very end and fix it.
+    '''
+
+    def test_chrome(self):
+        self.driver = webdriver.Chrome()
+        self._perform_test()
+
+    def test_firefox(self):
+        self.driver = webdriver.Firefox()
+        self._perform_test()
+
+    def _perform_test(self):
+        try:
+            self._perform_get()
+            self._assert_id_is_not_displayed('you-win')
+            self._perform_clicks_1()
+            self._assert_id_is_not_displayed('you-win')
+            self._perform_clicks_2()
+            self._assert_id_is_displayed('you-win')
+        finally:
+            self.driver.quit()
+
+    def _perform_clicks_1(self):
+        element_ids = (
+            'start-button',
+            'tile-a.0',
+            'tile-ae.0',
+            'tile-ae.1',
+            'tile-am',
+            'tile-a.1',
+            'tile-ae.2',
+            'tile-arum',
+            'tile-is.0',
+            'tile-is.1',
+            'tile-as',
+        )
+        for element_id in element_ids:
+            self.driver.find_element_by_id(element_id).click()
+
+    def _perform_clicks_2(self):
+        element_ids = (
+            'ablative-plural',
+            'accusative-plural',
             'tile-as',
             'tile-is.1',
         )
